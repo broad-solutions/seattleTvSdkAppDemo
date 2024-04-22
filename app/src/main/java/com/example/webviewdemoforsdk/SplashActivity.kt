031ded3e82fb8814fa9c4a8134576feea6381f6f
lixiaohong <617644384@qq.com>
2024-04-22 10:48:39 +0800

    dev_test 开发分支

--- app/src/main/java/com/example/webviewdemoforsdk/SplashActivity.kt
+++ app/src/main/java/com/example/webviewdemoforsdk/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.example.webviewdemoforsdk
 
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,7 @@ import com.cloudinfinitegroup.seattle_tv_sdk.DefaultAdListener
 import com.cloudinfinitegroup.seattle_tv_sdk.TvAdSdk
 import com.example.webviewdemoforsdk.databinding.ActivitySplashBinding
 import com.google.ads.interactivemedia.v3.api.AdEvent
+import com.google.android.gms.ads.AdSize
 import com.google.gson.Gson
 import javax.security.auth.login.LoginException
 
@@ -36,34 +38,54 @@ class SplashActivity : AppCompatActivity() {
             val accessTokenResponse = Gson().fromJson(it, Datautils::class.java)
             val tokenRequest = accessTokenResponse.access_token
             GlobalVariables.token = tokenRequest
-            TvAdSdk.init(tokenRequest, packageName, true)
-            mBinding.tvSdkView.startAd(
-                TvAdSdk.AdType.SPLASH,
-                listener = object : DefaultAdListener() {
-
-                    override fun onAdPaused() {
-                        super.onAdPaused()
-                        goToMain()
-                    }
-
-                    override fun onSkip() {
-                        goToMain()
-                    }
-
-                    override fun onAdCompleted() {
-                        goToMain()
-                    }
-
-                    override fun onAdFailedToLoad(errorMessage: String) {
-                        goToMain()
-                    }
-
-                    override fun onAdIsEmpty(errorMessage: String) {
-                        goToMain()
-                    }
-
+            TvAdSdk.init(this, tokenRequest, packageName, true) { result ->
+                result.adapterStatusMap.forEach {
+                    Log.d("TAG", "onCreate: ${it.key}")
+                    Log.d("TAG", "onCreate: ${it.value}")
                 }
-            )
+                val width = Resources.getSystem().displayMetrics.heightPixels
+                val size: AdSize =
+                    AdSize.getLandscapeInlineAdaptiveBannerAdSize(this@SplashActivity, width)
+                Log.e("splash", "width:$width,size:$size")
+                mBinding.mobileSdk.loadAd {
+                    adUnitId = "/6499/example/banner"  //固定式横幅
+//                    adUnitId = "/6499/example/interstitial" 插槽
+//                    adUnitId = "/6499/example/app-open"
+                    adType = TvAdSdk.MobilAdType.BANNER
+                    openAd = {
+                        it?.show(this@SplashActivity)
+                    }
+                }
+
+            }
+
+//            mBinding.tvSdkView.startAd(
+//                TvAdSdk.AdType.SPLASH,
+//                listener = object : DefaultAdListener() {
+//
+//                    override fun onAdPaused() {
+//                        super.onAdPaused()
+//                        goToMain()
+//                    }
+//
+//                    override fun onSkip() {
+//                        goToMain()
+//                    }
+//
+//                    override fun onAdCompleted() {
+//                        goToMain()
+//                    }
+//
+//                    override fun onAdFailedToLoad(errorMessage: String) {
+//                        goToMain()
+//                    }
+//
+//                    override fun onAdIsEmpty(errorMessage: String) {
+//                        goToMain()
+//                    }
+//
+//                }
+//            )
         }
     }
 
